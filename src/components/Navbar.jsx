d9550a94b6d1fc0403b138fe86bcfd55087016e1
Dalia Marzouk <69721475+daliaMarzouk90@users.noreply.github.com>
2025-08-17 23:00:38 +0300

    just a static page

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+    return (
+        <header>
+            <nav> 
+                <img className="img" src="src/assets/react-1.svg" alt="React Logo" />
+                <span>ReactFacts</span>
+            </nav>
+        </header>
+    )
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,9 @@
 import { createRoot } from 'react-dom/client'
-import { Fragment } from 'react'
-
-import { Header } from './header.jsx'
-import { Footer } from './footer.jsx'
-import { Main } from './main.jsx'
 
 const root = createRoot(document.getElementById('root'))
 
-import '/src/index.css'
+import App from './App.jsx'
 
 root.render(
-    <Fragment>
-        <Header />
-        <Main />
-        <Footer />
-    </Fragment>
-)    
+    <App />
+)
